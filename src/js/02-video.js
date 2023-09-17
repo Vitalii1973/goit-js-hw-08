f4704f6fbd9c2c779e11664b33847769215dcc93
Vitalii1973 <protsyukv@ukr.net>
2023-09-17 21:02:22 +0300

    dz-2-homme

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+// Отримуємо посилання на плеєр
+const vimeoPlayer = new Player('vimeo-player');
+
+// Отримуємо посилання на HTML елемент плеєра
+const iframe = document.querySelector('#vimeo-player');
+
+// Функція для збереження часу відтворення у локальне сховище
+function saveCurrentTime(currentTime) {
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+// Функція для завантаження часу відтворення з локального сховища
+function loadCurrentTime() {
+  return parseFloat(localStorage.getItem('videoplayer-current-time')) || 0;
+}
+
+// Відстежуємо подію timeupdate для оновлення часу відтворення
+vimeoPlayer.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    saveCurrentTime(seconds);
+  }, 1000)
+); // Оновлюємо не частіше, ніж раз на секунду
+
+// Отримуємо збережений час відтворення та встановлюємо його
+const savedTime = loadCurrentTime();
+vimeoPlayer
+  .setCurrentTime(savedTime)
+  .then(() => {
+    // Після встановлення часу відтворення, запускаємо відтворення
+    vimeoPlayer.play();
+  })
+  .catch(error => {
+    console.error('Unable to set the current time:', error);
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,58 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message"]');
+
+// Отримуємо збережений стан з локального сховища
+const savedState =
+  JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+
+// Заповнюємо поля форми зі збереженого стану
+if (savedState.email) {
+  emailInput.value = savedState.email;
+}
+
+if (savedState.message) {
+  messageTextarea.value = savedState.message;
+}
+
+// Функція для збереження стану форми в локальне сховище
+const saveState = () => {
+  const state = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(state));
+};
+
+// Відстежуємо подію input на полях форми та зберігаємо стан
+emailInput.addEventListener(
+  'input',
+  throttle(() => {
+    saveState();
+  }, 500)
+);
+
+messageTextarea.addEventListener(
+  'input',
+  throttle(() => {
+    saveState();
+  }, 500)
+);
+
+// Обробник події submit форми
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+
+  // Очищаємо локальне сховище
+  localStorage.removeItem('feedback-form-state');
+
+  // Виводимо дані форми в консоль
+  console.log(formData);
+});
